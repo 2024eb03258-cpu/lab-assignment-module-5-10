f9418fa37a765f3f4c5dbba2f0189fd9a1654c99
Nida Fatima <2024eb03258@online.bits-pilani.ac.in>
2026-02-05 16:48:06 +0300

    Create count_files.sh

---
+++ Question 3/count_files.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <directory>"
+    exit 1
+fi
+
+dir="$1"
+
+if [ ! -d "$dir" ]; then
+    echo "Directory does not exist."
+    exit 1
+fi
+
+count=$(ls -1 "$dir" | wc -l)
+
+echo "Number of files in $dir: $count"
